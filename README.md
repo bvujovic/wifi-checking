121f8b033ad28eb6be314544eeb7b1c085c75670
bvujovic <bv.net@outlook.com>
2024-05-03 22:05:25 +0200

    Load network credentials from file Check connectivity for every wi-fi network Report via WhatsApp message Add TODO list for this project to README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WiFi Checking
 Checks connectivity for multiple WiFi networks every x minutes.
+
+## TODO
+
+### SW
+- [x] Reading network credentials from csv file
+- [x] Checking connectivity for every wi-fi network
+- [x] Reporting via WhatsApp message
+    - [ ] HTML encode names of bad networks before sending to WA
+    - [ ] Don't report network every time it's bad - only when it went from good to bad (and from bad to good)
+- [ ] Add disconnect function and call it after successful connects
+- [ ] Add one or two retries to check if a problem with some wifi is temporary
+- [ ] Logging changes in network statuses (current time needed)
+
+### HW
+- [ ] Can this device "steal" power from the router (step down converter 12V->5V)?
+- [ ] Can this device reset router via transistor or relay?
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <CredCallMeBot.h>
+#include <CredCallMeBot.h> // CMB_PHONE and CMB_API_KEY defined
+#include "LinkedList.h"
+#include <LittleFS.h>
 
 #include "Blinky.h" // https://github.com/bvujovic/ArduinoLibs/tree/main/Blinky
 Blinky led = Blinky::create();
@@ -17,14 +19,17 @@ Blinky led = Blinky::create();
 #endif
 
 // Sending an actual WhatsApp message or not (testing purposes).
-#define REAL_SEND_MSG false
+#define REAL_SEND_MSG true
 
 // Milliseconds in 1 second.
 #define SEC (1000)
 // Milliseconds in 1 minute.
 #define MIN (60 * SEC)
 
-bool connectToWiFi(const char *ssid, const char *pass)
+// Deep sleep time in microseconds.
+#define SLEEP_INTERVAL 1000 * (DEBUG ? 15 * SEC : 5 * MIN)
+
+bool connectToWiFi(String ssid, String pass)
 {
   Serial.print("Connecting to ");
   Serial.print(ssid);
@@ -41,16 +46,18 @@ bool connectToWiFi(const char *ssid, const char *pass)
   return WiFi.isConnected();
 }
 
-int sendWhatsAppMessage()
+/// @brief Sends WhatsApp message about failed WiFi networks.
+/// @param ssids Names of failed WiFi networks.
+/// @return HTTP response code for sent message.
+int sendWhatsAppMessage(String ssids)
 {
   writeln("sendWhatsAppMessage");
 #if REAL_SEND_MSG
   //* https://www.callmebot.com/blog/free-api-whatsapp-messages/
   String url = "http://api.callmebot.com/whatsapp.php?";
   url = url + "phone=" + CMB_PHONE;
-  // 💥Stan, kuhinja, sudopera:
-  // VISOK NIVO VODE U SUDOPERI 💦
-  url = url + "&text=" + "%F0%9F%92%A5+Stan,+kuhinja,+sudopera:%0AVISOK+NIVO+VODE+U+SUDOPERI+%F0%9F%92%A6";
+  // https://www.callmebot.com/blog/test-whatsapp-api/ 📶 WiFi mreže: Sveti Sava, Tech... ne rade.
+  url = url + "&text=" + "📶%20WiFi%20mreže:%20" + ssids + "%20ne%20rade.";
   url = url + "&apikey=" + CMB_API_KEY;
   writeln(url);
   WiFiClient wiFiClient;
@@ -70,15 +77,92 @@ int sendWhatsAppMessage()
 #endif
 }
 
+enum CheckResult
+{
+  NotChecked,
+  Good,
+  Bad
+};
+
+struct Net
+{
+  String ssid;
+  String pass;
+  CheckResult result;
+};
+
+/// @brief Reads data about wifi networks from data/nets.csv.
+/// @return List of networks that should be checked.
+LinkedList<Net *> *readNetworkData()
+{
+  LinkedList<Net *> *nets = new LinkedList<Net *>();
+  File f = LittleFS.open("nets.csv", "r");
+  if (f)
+  {
+    while (f.available())
+    {
+      String line = f.readStringUntil('\n');
+      if (line.length() == 0)
+        continue;
+      // e.g. "1+WiFiName+WiFiPass"
+      if (line[0] == '1')
+      {
+        int idx1 = line.indexOf('+');
+        if (idx1 == -1)
+          continue;
+        int idx2 = line.indexOf('+', idx1 + 1);
+        if (idx2 == -1 || idx1 >= idx2)
+          continue;
+        Net *net = new Net{line.substring(idx1 + 1, idx2), line.substring(idx2 + 1, line.length() - 1), NotChecked};
+        nets->add(net);
+      }
+    }
+    f.close();
+  }
+  return nets;
+}
+
 void setup()
 {
   serialDebugBegin();
+  writeln("\n***START***");
   pinMode(led.getPin(), OUTPUT);
+  LittleFS.begin();
 
-  // prolazak kroz listu wifi mreza koje treba proveriti
-  // proveravanje svake od njih
-  // obavestavanje o problemu, ako postoji
-  // spavanje x minuta
+  writeln("Checking WiFi networks...");
+  LinkedList<Net *> *nets = readNetworkData();
+  Net *goodNet = NULL;
+  String badNets = "";
+  for (int i = 0; i < nets->size(); i++)
+  {
+    Net *net = nets->get(i);
+    writeln(net->ssid);
+    bool success = connectToWiFi(net->ssid, net->pass);
+    writeln(success);
+    net->result = success ? Good : Bad;
+    if (success)
+    {
+      if (goodNet == NULL)
+        goodNet = net;
+    }
+    else
+      badNets += (badNets != "" ? ", " : "") + net->ssid;
+  }
+  writeln(badNets);
+
+  // if there are bad networks to report on and at least one good network to be able to send message
+  if (badNets != "" && goodNet != NULL)
+  {
+    writeln("Sending WA msg...");
+    bool success = connectToWiFi(goodNet->ssid, goodNet->pass);
+    if (success)
+      sendWhatsAppMessage(badNets);
+    else
+      ESP.reset();
+  }
+  
+  writeln("Sleep...");
+  ESP.deepSleep(SLEEP_INTERVAL);
 }
 
 void loop()
